1ca725833047f2c1b8231929db0544bf8240bebd
Yurii Itter <alexia.revoluzioner@gmail.com>
2020-03-05 15:33:11 +0200

    database module minor updates

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MongooseAutoIncrementID } from 'mongoose-auto-increment-reworked';
+
 import { DatabaseService } from './database.service';
 
 import { ChatSchema } from './schemas/chat.schema';
 
 @Module({
-    imports: [MongooseModule.forFeatureAsync([
-        {
-            name: 'Chat',
-            useFactory: () => {
-                const schema = ChatSchema;
-                schema.plugin(MongooseAutoIncrementID.plugin, { modelName: 'Chat' });
-                return schema;
-            },
-        },
+    imports: [MongooseModule.forFeature([
+        { name: 'Chat', schema: ChatSchema }
     ])],
     providers: [DatabaseService],
     exports: [DatabaseService]
