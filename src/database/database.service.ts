13c85995796a93fb891b699e00b64a751bc475a1
Yurii Itter <alexia.revoluzioner@gmail.com>
2020-03-05 21:16:04 +0200

    minor database module fixes, cryptocurrencies module was inited

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -4,15 +4,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { ChatInterface } from './interfaces/chat.interface';
-import { MessageInterface } from '../message/interfaces/message.interface';
+
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @Injectable()
 export class DatabaseService {
 
     constructor(@InjectModel('Chat') private readonly chatModel: Model<ChatInterface>) { }
 
-    public async ensureChat({ chatId, fullName, lang }: MessageInterface): Promise<ChatInterface> {
-        
+    public async ensureChat({ chatId, fullName, lang }: CreateChatDto): Promise<ChatInterface> {
+
         let chat: ChatInterface = await this.chatModel.findOne({ chatId });
 
         if (chat) { return chat }
